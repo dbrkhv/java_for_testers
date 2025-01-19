fc6c438a2d4c83f3654ff52b7449be337031360a
dbrkhv <dbrukhovetskaya@gmail.com>
2025-01-19 22:15:12 +0300

    Add square tests

--- sandbox/src/main/java/ru/jft/geometry/Geometry.java
+++ sandbox/src/main/java/ru/jft/geometry/Geometry.java
@@ -5,9 +5,9 @@ import ru.jft.geometry.figures.Square;
 
 public class Geometry {
     public static void main(String[] args) {
-        Square.printSquareArea(7.0);
-        Square.printSquareArea(5.0);
-        Square.printSquareArea(3.0);
+        Square.printArea(7.0);
+        Square.printArea(5.0);
+        Square.printArea(3.0);
 
         Rectangle.printRectangleArea(3.0, 5.0);
         Rectangle.printRectangleArea(7.0, 9.0);
--- sandbox/src/main/java/ru/jft/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/jft/geometry/figures/Square.java
@@ -1,12 +1,17 @@
 package ru.jft.geometry.figures;
 
 public class Square {
-    public static void printSquareArea(double side) {
-        String text = String.format("Площадь квадрата со стороной %f = %f", side, squareArea(side));
+    public static void printArea(double side) {
+        //%f - вещественное число: float, double
+        String text = String.format("Площадь квадрата со стороной %f = %f", side, area(side));
         System.out.println(text);
     }
 
-    private static double squareArea(double a) {
+    public static double area(double a) {
         return a * a;
+    }
+
+    public static double perimetr(double a) {
+        return 4.0 * a;
     }
 }
