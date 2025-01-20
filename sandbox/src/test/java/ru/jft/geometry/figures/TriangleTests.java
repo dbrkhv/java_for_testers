19afe540fa660e7c4b61612b9362e5dbf61bfa2b
dbrkhv <dbrukhovetskaya@gmail.com>
2025-01-20 13:24:41 +0300

    Triangle tests p2

--- sandbox/src/test/java/ru/jft/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/jft/geometry/figures/TriangleTests.java
@@ -7,14 +7,20 @@ public class TriangleTests {
 
     @Test
     void checkCalculatePerimeter() {
-        Triangle t1 = new Triangle(3.0,4.0,5.0);
-        Assertions.assertEquals(12.0, Triangle.calculatePerimeter(t1));
+        Triangle t1 = new Triangle(5.0, 12.0, 13.0);
+        Assertions.assertEquals(30.0, Triangle.calculatePerimeter(t1));
+
+        Triangle t2 = new Triangle(10.0, 10.0, 10.0);
+        Assertions.assertEquals(30.0, Triangle.calculatePerimeter(t2));
     }
 
     @Test
-    void checkCalculateArea(){
-        Triangle t1 = new Triangle(3.0,4.0,5.0);
-        Assertions.assertEquals(12.0, Triangle.calculatePerimeter(t1));
+    void checkCalculateArea() {
+        Triangle t1 = new Triangle(5.0, 12.0, 13.0);
+        Assertions.assertEquals(30.0, Triangle.calculatePerimeter(t1));
+
+        Triangle t2 = new Triangle(10.0, 10.0, 10.0);
+        Assertions.assertEquals(43.30127, Triangle.calculatePerimeter(t2));
     }
 
-}
+
